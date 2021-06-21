1023d3e52f0304229beabcf596a728b2a3d51514
benshmuel <bshmuel19@gmail.com>
2021-06-21 23:47:08 +0300

    intial support for WebView.

--- app/src/main/java/com/example/binahoverflow/ui/fragment/BinahWebViewFragment.kt
+++ app/src/main/java/com/example/binahoverflow/ui/fragment/BinahWebViewFragment.kt
@@ -1,7 +1,29 @@
 package com.example.binahoverflow.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.binahoverflow.databinding.BinahWebViewFragmentLayoutBinding
 
 class BinahWebViewFragment: Fragment() {
 
+    private lateinit var binding: BinahWebViewFragmentLayoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        binding = BinahWebViewFragmentLayoutBinding.inflate(inflater,container,false)
+
+        val urlFromRecycler = arguments?.getString("URL") ?: "https://stackoverflow.com"
+
+        binding.binahWebView.loadUrl(urlFromRecycler)
+
+        return binding.root
+    }
 }
